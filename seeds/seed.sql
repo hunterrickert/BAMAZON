008aac4ad6605c8fe14dd3f9054ac19aef5ffd87
Hunter Rickert <hunter.rickert@gmail.com>
2019-08-19 23:42:40 -0700

    creating connection to db

---
+++ seeds/seed.sql
@@ -0,0 +1,31 @@
+USE bamazon_db
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+Values ("Pillow", "Home", 20, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+Values ("Comforter", "Home", 40, 25);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+Values ("Bed Sheets", "Home", 25, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+Values ("Towels", "Home", 15, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+Values ("Shower Curtain", "Home", 10, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+Values ("Frying Pan", "Cookware", 35, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+Values ("Blender", "Cookware", 75, 25);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+Values ("Plates", "Cookware", 30, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+Values ("Lawn Mower", "Landscaping", 200, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+Values ("Weed Wacker", "Landscaping", 120, 50);
