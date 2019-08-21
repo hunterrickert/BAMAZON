d1fdd8835dd76ae1145ef72656fa8c7590a80422
Hunter Rickert <hunter.rickert@gmail.com>
2019-08-21 09:14:22 -0700

    complete prompt, ux

--- amazon.js
+++ amazon.js
@@ -17,24 +17,76 @@ connection.connect(function(err) {
     if(err) throw err;
     console.log("connected to BAMAZON")
     connection.threadId + "\n";
-    displayProducts();
+    // displayProducts();
+    purchase();
 });
 
-function displayProducts() {
-    connection.query("SELECT * FROM products", 
-    function(err, res) {
-        if(err) throw err;
-        // console.table(res);
-        // connection.end();
-    });
-};
+// function displayProducts() {
+//     connection.query("SELECT * FROM products", 
+//     function(err, res) {
+//         if(err) throw err;
+//         console.table(res);
+//         // connection.end();
+//     });
+// };
 
-inquirer.prompt([
-    {
-    type: "list",
-    message: "What item would you like to purchase?",
-    choices: choices,
-    name:"productChoices"
-    }
-])
+function purchase() {
+    connection.query("SELECT * FROM products", function(err, results) {
+        if(err) throw err;
+    
+    console.log("hunter!!!");
+    inquirer.
+        prompt([
+            {
+            type: "rawlist",
+            message: "Enter the Item Id of the product you would like to purchase?",
+            choices: function () {
+                var choiceArray = [];
+                for (var i = 0; i < results.length; i++) {
+                    choiceArray.push(results[i].product_name);
+                }
+                return choiceArray;
+            },
+            name:"productChoices"
+            },
+            {
+                type: "input",
+                message: "How many would you like to buy?",
+                name: "purchaseAmount"
+            }
+
+    ]).then(function(answer) {
+        var chosenItem;
+        for (var i = 0; i < results.length; i++) {
+            if (results[i].product_name === answer.productChoices) {
+              chosenItem = results[i];
+            }
+          };
+          if(chosenItem.stock_quantity >= answer.purchaseAmount) {
+              var newStockQuantity = chosenItem.stock_quantity - parseInt(answer.purchaseAmount);
+              console.log(newStockQuantity);
+              connection.query("UPDATE products SET ? WHERE ?", 
+              [
+                {
+                      stock_quantity: newStockQuantity 
+                    },
+                {
+                    item_id: chosenItem.item_id
+
+                }
+            ], function(err){
+                if(err) throw err;
+                console.log("Purchase was successful!!")
+                purchase();
+            }
+            );
+          }else{
+              console.log("Sorry, out of stock!")
+              purchase();
+          }
+
+    })
+
+})
+}
 
